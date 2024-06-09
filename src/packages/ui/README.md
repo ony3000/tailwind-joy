03b48d13f1d7282370f88ff8b5f89b9f34bd17e4
Hyeonjong <nianelo4@gmail.com>
2024-06-09 16:13:17 +0900

    Move library related things

---
+++ src/packages/ui/README.md
@@ -0,0 +1,6 @@
+[Joy UI](https://mui.com/joy-ui/getting-started/)의 component를 Tailwind CSS 방식으로 구현한 React component library
+
+## 개발 환경
+
+- Node.js 18
+- [pnpm 8.5.1](https://pnpm.io/)
--- src/packages/ui/vite.config.mjs
+++ src/packages/ui/vite.config.mjs
@@ -5,19 +5,19 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, './index.ts'),
       name: 'DemoLib',
       fileName: 'demo-lib',
     },
   },
   plugins: [
     dts({
-      include: ['src/**'],
+      include: ['./**'],
     }),
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': resolve(__dirname, '.'),
     },
   },
 });
