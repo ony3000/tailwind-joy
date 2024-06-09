1f7064db986e91b59993169651a0c876969affb6
Hyeonjong <nianelo4@gmail.com>
2024-06-09 17:33:19 +0900

    Use ui workspace as dependency

--- src/apps/playground/App.tsx
+++ src/apps/playground/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { components } from '@ony3000/experimental-ui';
+import { Button } from '@ony3000/experimental-ui/components';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,13 +12,21 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>console에 HMR 실패 에러가 뜨면 화면 새로고침 해보기</p>
+      <div>
+        <h2>import test</h2>
+        <div>
+          <h3>root entry point에서 가져오기</h3>
+          <components.Button>dolor sit amet</components.Button>
+        </div>
+        <div>
+          <h3>
+            <code>/components</code> entry point에서 가져오기
+          </h3>
+          <Button>Lorem ipsum</Button>
+        </div>
+      </div>
     </>
   );
 }
