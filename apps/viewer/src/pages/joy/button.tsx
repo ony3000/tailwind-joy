b160616ccd847f9e5da2b9d5a9262e6e6b90de57
Hyeonjong <nianelo4@gmail.com>
2024-06-25 20:30:55 +0900

    Split button section into two pages

---
+++ apps/viewer/src/pages/joy/button.tsx
@@ -0,0 +1,126 @@
+import { Fragment } from 'react';
+import { Box, Button, Card, Typography } from '@mui/joy';
+import AddIcon from '@mui/icons-material/Add';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+
+const sizes = ['sm', 'md', 'lg'] as const;
+
+const variants = ['solid', 'soft', 'outlined', 'plain'] as const;
+
+const colors = ['primary', 'neutral', 'danger', 'success', 'warning'] as const;
+
+const customs: { title: string; props: Record<string, any> }[] = [
+  {
+    title: 'default',
+    props: {},
+  },
+  {
+    title: 'startDecorator',
+    props: {
+      startDecorator: <AddIcon />,
+    },
+  },
+  {
+    title: 'endDecorator',
+    props: {
+      endDecorator: <KeyboardArrowRightIcon />,
+    },
+  },
+  {
+    title: 'fullWidth',
+    props: {
+      fullWidth: true,
+    },
+  },
+  {
+    title: 'loading',
+    props: {
+      loading: true,
+    },
+  },
+  {
+    title: 'loadingIndicator',
+    props: {
+      loading: true,
+      loadingIndicator: 'Loading...',
+    },
+  },
+  {
+    title: 'loadingPosition: start',
+    props: {
+      loading: true,
+      loadingPosition: 'start',
+    },
+  },
+  {
+    title: 'loadingPosition: end',
+    props: {
+      loading: true,
+      loadingPosition: 'end',
+      endDecorator: <KeyboardArrowRightIcon />,
+    },
+  },
+];
+
+export default function JoyButtonPage() {
+  return (
+    <Box>
+      <Typography level="h1" sx={{ mb: 2 }}>
+        Button
+      </Typography>
+      <div className="grid grid-cols-1 gap-4 min-[920px]:grid-cols-2 min-[1380px]:grid-cols-3 min-[1840px]:grid-cols-4">
+        {customs.map(({ title, props }) => (
+          <Fragment key={title}>
+            {colors.map((color) => (
+              <Fragment key={color}>
+                <Card variant="outlined">
+                  {variants.map((variant) => (
+                    <div key={variant} className="grid grid-cols-3 gap-4">
+                      {sizes.map((size) => (
+                        <div
+                          key={size}
+                          className="flex items-center justify-center"
+                        >
+                          <Button
+                            size={size}
+                            variant={variant}
+                            color={color}
+                            {...props}
+                          >
+                            Button
+                          </Button>
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </Card>
+                <Card variant="outlined">
+                  {variants.map((variant) => (
+                    <div key={variant} className="grid grid-cols-3 gap-4">
+                      {sizes.map((size) => (
+                        <div
+                          key={size}
+                          className="flex items-center justify-center"
+                        >
+                          <Button
+                            size={size}
+                            variant={variant}
+                            color={color}
+                            {...props}
+                            disabled
+                          >
+                            Button
+                          </Button>
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </Card>
+              </Fragment>
+            ))}
+          </Fragment>
+        ))}
+      </div>
+    </Box>
+  );
+}
--- apps/viewer/src/sections/index.ts
+++ apps/viewer/src/sections/index.ts
@@ -1,3 +1,2 @@
-export { default as ButtonSection } from './ButtonSection';
 export { default as CircularProgressSection } from './CircularProgressSection';
 export { default as IconButtonSection } from './IconButtonSection';
