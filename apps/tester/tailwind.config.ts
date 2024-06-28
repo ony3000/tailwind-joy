0b5cc88145fc8f1a0cd1a1f834288c22527aaca4
Hyeonjong <nianelo4@gmail.com>
2024-06-28 22:32:07 +0900

    Update tailwind.config.ts

--- apps/tester/tailwind.config.ts
+++ apps/tester/tailwind.config.ts
@@ -4,7 +4,11 @@ import { joyPreset } from 'tailwind-joy/theme';
 export default {
   presets: [joyPreset],
   darkMode: 'selector',
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/tailwind-joy/**'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './tests/**/*.{js,ts,jsx,tsx}',
+    './node_modules/tailwind-joy/**',
+  ],
   theme: {
     extend: {},
   },
