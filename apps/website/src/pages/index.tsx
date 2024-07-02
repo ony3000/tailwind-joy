efe2df0a86ffb418d869e7f3b7720e50a04770da
Hyeonjong <nianelo4@gmail.com>
2024-07-02 23:47:51 +0900

    Remove broken link

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -1,16 +1,13 @@
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
-import { Button } from 'tailwind-joy/components';
-
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -18,29 +15,18 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link to="/docs/intro">
-            <Button
-              size="lg"
-              color="neutral"
-              variant="soft"
-              className="font-[inherit]"
-            >
-              Docusaurus Tutorial - 5min ⏱️
-            </Button>
-          </Link>
-        </div>
       </div>
     </header>
   );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
