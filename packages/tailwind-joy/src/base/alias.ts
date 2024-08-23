cf2287826491b72d5f0d194a01fb1dbffae4226a
Hyeonjong <nianelo4@gmail.com>
2024-08-23 23:54:32 +0900

    Use custom random number generator to generate uuid
    This bypasses the 'crypto.getRandomValues() not supported' error that occurs when building a website.

--- packages/tailwind-joy/src/base/alias.ts
+++ packages/tailwind-joy/src/base/alias.ts
@@ -1,3 +1,13 @@
-export { v4 as uuid } from 'uuid';
+import { v4 } from 'uuid';
 
 export const r = String.raw;
+
+function naiveRng() {
+  return [...Array(16)].map(() => Math.floor(Math.random() * 0x100));
+}
+
+export function uuid() {
+  return v4({
+    rng: naiveRng,
+  });
+}
