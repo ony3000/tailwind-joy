cf47f6e6019334f7a59e6d6d6f4c8c8b041ee606
Hyeonjong <nianelo4@gmail.com>
2024-08-23 23:24:20 +0900

    Refactor chip-based components

--- apps/website/src/components/docs/AvailableFrom.tsx
+++ apps/website/src/components/docs/AvailableFrom.tsx
@@ -1,15 +1,9 @@
-import { Chip } from '@mui/joy';
-
-type AvailableFromProps = {
-  version: string;
-};
-
-export function AvailableFrom({ version }: AvailableFromProps) {
+export function AvailableFrom({ version }: { version: string }) {
   return (
     <p>
-      <Chip sx={{ '--Chip-radius': '8px' }}>
+      <span className="bg-joy-neutral-200 text-joy-neutral-700 dark:bg-joy-neutral-500 dark:text-joy-common-white min-h-6 relative inline-flex max-w-max items-center justify-center gap-1 rounded-lg px-2 align-middle text-sm font-medium">
         Available from version {version}
-      </Chip>
+      </span>
     </p>
   );
 }
