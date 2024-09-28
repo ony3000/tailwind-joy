3c97ebbb26586a6cec7e9a2ba8eca157161927a7
Hyeonjong <nianelo4@gmail.com>
2024-09-28 18:15:59 +0900

    Complements box component

--- packages/tailwind-joy/src/components/Box.tsx
+++ packages/tailwind-joy/src/components/Box.tsx
@@ -1,38 +1,33 @@
 import { clsx } from 'clsx';
-import type { ComponentProps, FunctionComponent, ComponentClass } from 'react';
 import { forwardRef, createElement } from 'react';
 import { twMerge } from 'tailwind-merge';
-import type { BaseVariants, GeneratorInput } from '@/base/types';
+import type {
+  BaseVariants,
+  GeneratorInput,
+  GenericComponentPropsWithVariants,
+} from '@/base/types';
 
 function boxRootVariants(props?: BaseVariants) {
   return twMerge(clsx(['tj-box-root group/tj-box']));
 }
 
-interface BoxRootVariants {
-  component?: string | FunctionComponent | ComponentClass;
-}
+interface BoxRootVariants {}
 
-type BoxRootProps = Omit<ComponentProps<'div'>, keyof BoxRootVariants> &
-  BoxRootVariants;
+type BoxRootProps = GenericComponentPropsWithVariants<'div', BoxRootVariants>;
 
-export const Box = forwardRef<HTMLElement, BoxRootProps>(function BoxRoot(
-  { children, className, component = 'div', ...otherProps },
+export const Box = forwardRef(function BoxRoot(
+  { children, className, component = 'div', ...otherProps }: BoxRootProps,
   ref,
 ) {
-  return typeof component === 'string'
-    ? createElement(
-        component,
-        {
-          ref,
-          className: twMerge(boxRootVariants(), className),
-          ...otherProps,
-        },
-        children,
-      )
-    : createElement(component, {
-        // @ts-expect-error
-        ref,
-      });
+  return createElement(
+    component,
+    {
+      ref,
+      className: twMerge(boxRootVariants(), className),
+      ...otherProps,
+    },
+    children,
+  );
 });
 
 export const generatorInputs: GeneratorInput[] = [
