7b441c3b3d0dfd4b04d7b6f7597319dfc4ba1219
Hyeonjong <nianelo4@gmail.com>
2024-11-16 21:47:56 +0900

    Remove vite-plugin-dts and related configs

--- packages/tailwind-joy/vite.config.mjs
+++ packages/tailwind-joy/vite.config.mjs
@@ -1,8 +1,6 @@
 import react from '@vitejs/plugin-react';
-import { writeFileSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 import { safelistGenerator } from './src/plugins/safelist-generator';
 
 export default defineConfig({
@@ -20,25 +18,7 @@ export default defineConfig({
       external: ['react', 'react/jsx-runtime', 'tailwindcss'],
     },
   },
-  plugins: [
-    dts({
-      entryRoot: 'src',
-      include: ['src/**'],
-      afterBuild(emittedFiles) {
-        for (const [path, code] of emittedFiles) {
-          const extensionReplacedPath = path.replace(/\.d\.ts$/, '.d.cts');
-          const extensionAddedCode = code.replace(
-            /(from '\.?\.(?:\/[^/;]+)*\/[^/;.]+)(';)/g,
-            '$1.cjs$2',
-          );
-
-          writeFileSync(extensionReplacedPath, extensionAddedCode);
-        }
-      },
-    }),
-    react(),
-    safelistGenerator(),
-  ],
+  plugins: [react(), safelistGenerator()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
