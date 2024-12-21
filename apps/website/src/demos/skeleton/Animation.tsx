86e56deb5f29fba6f156176e300a8317c684c487
Hyeonjong <nianelo4@gmail.com>
2024-12-21 14:57:55 +0900

    Remove unnecessary containers

--- apps/website/src/demos/skeleton/Animation.tsx
+++ apps/website/src/demos/skeleton/Animation.tsx
@@ -1,6 +1,5 @@
 import {
   AspectRatio,
-  Box,
   Button,
   Sheet,
   Skeleton,
@@ -11,33 +10,30 @@ import { DisplayStand } from '@site/src/components/docs/DisplayStand';
 export function SkeletonAnimation() {
   return (
     <DisplayStand>
-      {/* TODO: Replace Box with Stack. */}
-      <Box className="w-full space-y-4">
-        {/* TODO: Replace Sheet with Card. */}
-        <Sheet
-          variant="outlined"
-          className="mx-auto flex w-full max-w-[343px] flex-col gap-y-3 rounded-lg p-4"
-        >
-          <AspectRatio ratio="21/9">
-            <Skeleton animation="wave" variant="overlay">
-              <img
-                alt=""
-                src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
-              />
-            </Skeleton>
-          </AspectRatio>
-          <Typography className="overflow-hidden">
-            <Skeleton animation="wave">
-              Lorem ipsum is placeholder text commonly used in the graphic,
-              print, and publishing industries.
-            </Skeleton>
-          </Typography>
-          <Button>
-            Read more
-            <Skeleton animation="wave" />
-          </Button>
-        </Sheet>
-      </Box>
+      {/* TODO: Replace Sheet with Card. */}
+      <Sheet
+        variant="outlined"
+        className="mx-auto flex w-full max-w-[343px] flex-col gap-y-3 rounded-lg p-4"
+      >
+        <AspectRatio ratio="21/9">
+          <Skeleton animation="wave" variant="overlay">
+            <img
+              alt=""
+              src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+            />
+          </Skeleton>
+        </AspectRatio>
+        <Typography className="overflow-hidden">
+          <Skeleton animation="wave">
+            Lorem ipsum is placeholder text commonly used in the graphic, print,
+            and publishing industries.
+          </Skeleton>
+        </Typography>
+        <Button>
+          Read more
+          <Skeleton animation="wave" />
+        </Button>
+      </Sheet>
     </DisplayStand>
   );
 }
--- apps/website/src/demos/skeleton/Loading.tsx
+++ apps/website/src/demos/skeleton/Loading.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import {
   AspectRatio,
-  Box,
   Button,
   Sheet,
   Skeleton,
@@ -14,37 +13,34 @@ export function SkeletonLoading() {
 
   return (
     <DisplayStand>
-      {/* TODO: Replace Box with Stack. */}
-      <Box className="w-full space-y-4">
-        {/* TODO: Replace Sheet with Card. */}
-        <Sheet
-          variant="outlined"
-          className="mx-auto flex w-full max-w-[343px] flex-col gap-y-3 rounded-lg p-4"
-        >
-          <AspectRatio ratio="21/9">
-            <Skeleton loading={loading} variant="overlay">
-              <img
-                alt=""
-                src={
-                  loading
-                    ? 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
-                    : 'https://images.unsplash.com/photo-1686548812883-9d3777f4c137?h=400&fit=crop&auto=format&dpr=2'
-                }
-              />
-            </Skeleton>
-          </AspectRatio>
-          <Typography>
-            <Skeleton loading={loading}>
-              {loading
-                ? 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.'
-                : 'An aerial view of a road in the middle of a forest. This image was uploaded by Dian Yu on Unsplash.'}
-            </Skeleton>
-          </Typography>
-          <Button onClick={() => setLoading(!loading)}>
-            {loading ? 'Stop' : 'Start'} loading
-          </Button>
-        </Sheet>
-      </Box>
+      {/* TODO: Replace Sheet with Card. */}
+      <Sheet
+        variant="outlined"
+        className="mx-auto flex w-full max-w-[343px] flex-col gap-y-3 rounded-lg p-4"
+      >
+        <AspectRatio ratio="21/9">
+          <Skeleton loading={loading} variant="overlay">
+            <img
+              alt=""
+              src={
+                loading
+                  ? 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
+                  : 'https://images.unsplash.com/photo-1686548812883-9d3777f4c137?h=400&fit=crop&auto=format&dpr=2'
+              }
+            />
+          </Skeleton>
+        </AspectRatio>
+        <Typography>
+          <Skeleton loading={loading}>
+            {loading
+              ? 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.'
+              : 'An aerial view of a road in the middle of a forest. This image was uploaded by Dian Yu on Unsplash.'}
+          </Skeleton>
+        </Typography>
+        <Button onClick={() => setLoading(!loading)}>
+          {loading ? 'Stop' : 'Start'} loading
+        </Button>
+      </Sheet>
     </DisplayStand>
   );
 }
