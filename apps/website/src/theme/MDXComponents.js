c93a3c0b2331e876414853fdb4bf8c6f8cbc9fa1
Hyeonjong <nianelo4@gmail.com>
2024-12-25 23:55:28 +0900

    Unregister some components from mdx components

--- apps/website/src/theme/MDXComponents.js
+++ apps/website/src/theme/MDXComponents.js
@@ -1,24 +1,10 @@
 import MDXComponents from '@theme-original/MDXComponents';
-import { CssVarsProvider } from '@mui/joy/styles';
-import { Button as JoyButton } from '@mui/joy';
-import { Box, Button, IconAdapter } from 'tailwind-joy/components';
 import { AvailableFrom } from '@site/src/components/docs/AvailableFrom';
 import { DeprecatedIn } from '@site/src/components/docs/DeprecatedIn';
 import { DisplayStand } from '@site/src/components/docs/DisplayStand';
 
 export default {
   ...MDXComponents,
-
-  // --------------------------------
-
-  CssVarsProvider,
-  JoyButton,
-
-  // --------------------------------
-
-  Box,
-  Button,
-  IconAdapter,
 
   // --------------------------------
 
